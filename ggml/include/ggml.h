dc9943753fea9443b05ba8c11e862affed432c43
HimariO <dsfhe49854@gmail.com>
2024-09-30 22:30:02 +0800

    Add vl-rope/2d-rope support for qwen2vl ViT

--- ggml/include/ggml.h
+++ ggml/include/ggml.h
@@ -1520,6 +1520,7 @@ extern "C" {
             struct ggml_tensor  * b,
             struct ggml_tensor  * c,
             int                   n_dims,
+            int                   sections[3],
             int                   mode,
             int                   n_ctx_orig,
             float                 freq_base,
